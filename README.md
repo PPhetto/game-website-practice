a637def5a6358196f8dff0e817e5af95de089c9a
Tanachot Ke <pphet.suityan@gmail.com>
2026-02-27 20:20:25 +0700

    game-website-practice

---
+++ README.md
@@ -0,0 +1,21 @@
+This website create for leaning HTML, CSS and Javascript
+
+## Note
+
+This website was created for learning and portfolio purposes only.
+
+It is a practice project where I tried to recreate an existing website layout.
+All images, logos, and content belong to their original owners.
+
+This project is not for commercial use and does not generate any profit.
+
+---
+
+## หมายเหตุ
+
+เว็บไซต์นี้จัดทำขึ้นเพื่อการศึกษาและใช้เป็นผลงาน (Portfolio) เท่านั้น
+
+เป็นโปรเจกต์ฝึกทำเว็บโดยลองทำตามรูปแบบของเว็บไซต์ที่มีอยู่จริง  
+รูปภาพ โลโก้ และเนื้อหาทั้งหมดเป็นของเจ้าของผลงานเดิม
+
+ไม่มีการนำไปใช้เชิงพาณิชย์หรือสร้างรายได้
---
+++ js/newsdata.js
@@ -0,0 +1,82 @@
+const newsData = [
+    {
+        id: 1,
+        category: "Game",
+        date: "2025.12.17 23:00",
+        title: "現在確認している不具合について"
+    },
+    {
+        id: 2,
+        category: "Game",
+        date: "2025.12.01 12:00",
+        title: "トゥインクルコレクション プリティーダービーガチャ開催！",
+        image: "announce_banner_3392.png"
+    },
+    {
+        id: 3,
+        category: "Media",
+        date: "2025.12.17 15:00",
+        title: "一部育成ウマ娘に進化スキル追加",
+        image: "banner_60300026.png"
+    },
+    {
+        id: 4,
+        category: "Game",
+        date: "2025.12.15 11:00",
+        title: "現在確認している不具合について"
+    },
+    {
+        id: 5,
+        category: "Game",
+        date: "2025.12.01 12:00",
+        title: "トゥインクルコレクション プリティーダービーガチャ開催！",
+        image: "announce_banner_3392.png"
+    },
+    {
+        id: 6,
+        category: "Media",
+        date: "2025.12.17 15:00",
+        title: "一部育成ウマ娘に進化スキル追加",
+        image: "banner_60300026.png"
+    },
+    {
+        id: 7,
+        category: "Game",
+        date: "2025.12.15 11:00",
+        title: "現在確認している不具合について"
+    },
+    {
+        id: 8,
+        category: "Game",
+        date: "2025.12.18 12:00",
+        title: "トゥインクルコレクション プリティーダービーガチャ開催！",
+        image: "announce_banner_3392.png"
+    },
+    {
+        id: 9,
+        category: "Game",
+        date: "2025.12.17 15:00",
+        title: "一部育成ウマ娘に進化スキル追加",
+        image: "banner_60300026.png"
+    },
+    {
+        id: 10,
+        category: "Game",
+        date: "2025.12.15 11:00",
+        title: "現在確認している不具合について"
+    },
+    {
+        id: 11,
+        category: "Game",
+        date: "2025.12.01 12:00",
+        title: "トゥインクルコレクション プリティーダービーガチャ開催！",
+        image: "announce_banner_3392.png"
+    },
+    {
+        id: 12,
+        category: "Game",
+        date: "2025.12.17 15:00",
+        title: "一部育成ウマ娘に進化スキル追加",
+        image: "banner_60300026.png"
+    },
+]
